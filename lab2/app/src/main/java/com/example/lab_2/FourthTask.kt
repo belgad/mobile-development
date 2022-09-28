67adfa70a2dedf5ccf5ba6fbc93423db50c05c80
Gleb Danshin <glebd79@gmail.com>
2022-09-28 10:59:47 +0300

    Lab 2

---
+++ lab2/app/src/main/java/com/example/lab_2/FourthTask.kt
@@ -0,0 +1,11 @@
+package com.example.lab_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FourthTask : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fourth_task)
+    }
+}
